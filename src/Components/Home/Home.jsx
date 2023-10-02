3ef61ba7c31852cb270164a72af2f1fc3cbb4c27
AlgoWebBot <shimulzahan636@gmail.com>
2023-10-03 00:40:49 +0600

    header part update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Header from '../Shared/Header'
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+            <Header />
+            <nav>
+                <div className='bg-[#F3F3F3] p-3 flex justify-center items-center'>
+                    <button className='btn bg-[#D72050] text-white font-medium capitalize text-lg px-6'>Latest</button>
+                    <div className='text-black text-xl flex justify-center items-center space-x-10'>
+                        <Marquee>
+                            <div>I can be a React component, multiple React components, or just some text.</div>
+                            <div className='ml-20'>I can be a React component</div>
+                        </Marquee>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Home
---
+++ src/Components/Shared/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import moment from 'moment'
+
+const Header = () => {
+
+
+  return (
+    <div className='py-8 text-center space-y-2'>
+      <img src="../../../public/logo.png" alt="" className='mx-auto' />
+      <p className='text-lg text-[#706F6F] font-normal'>Journalism Without Fear or Favour</p>
+      <p className='text-2xl text-[#414040]'>{moment().format("dddd, MMMM DD, YYYY") }</p>
+    </div>
+  )
+}
+
+export default Header
